1c89365b2e460273327f83457608d7c753823b0f
Павел Кокшаров <Enot_shotgun@mail.ru>
2020-09-09 11:09:46 +0500

    BYAK

---
+++ MCSD Study Guide/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCSD_Study_Guide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
