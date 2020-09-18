4365694a052ca5062e2027f9f250c4908dfe9c9e
Павел Кокшаров <Enot_shotgun@mail.ru>
2020-09-18 18:57:15 +0500

    Калькулятор

--- MCSD Study Guide/Program.cs
+++ MCSD Study Guide/Program.cs
@@ -6,12 +6,47 @@ using System.Threading.Tasks;
 
 namespace MCSD_Study_Guide
 {
-    class Program
+    class Calculator
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("f");
-
+            Operation();           
+        }       
+        static void Operation()
+        {            
+            Console.WriteLine("Press any following key to perform an arithmetic operation:" +
+                "1-Addittion " +
+                "2-Substraction " +
+                "3-Multipliation " +
+                "4-Division ");
+            int caseSwitch = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Value 1:");
+            int value1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Value 2:");
+            int value2 = int.Parse(Console.ReadLine());
+            Switch(caseSwitch, value1, value2);
+            Console.WriteLine("Do you want to continue again (Y/N)?");           
+            while (Console.ReadLine() == "Y")
+                Operation();
+        }
+        static void Switch(int caseSwitch, int value1, int value2)
+        {
+            switch (caseSwitch)
+            {
+                case 1:
+                    Console.WriteLine(value1 + value2);
+                    break;
+                case 2:
+                    Console.WriteLine(value1 - value2);
+                    break;
+                case 3:
+                    Console.WriteLine(value1 * value2);
+                    break;
+                case 4:
+                    Console.WriteLine(value1 / value2);
+                    break;
+            }
         }
     }
 }
+
